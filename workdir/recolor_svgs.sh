69d49549d5d2a2625f06231ca1f727663b902118
ATM Jahid Hasan <atmjahidhasan@gmail.com>
2025-02-06 14:20:12 -0500

    Update metadata.toml template
    Also, convert a third color using the recolor_svgs script.

--- workdir/recolor_svgs.sh
+++ workdir/recolor_svgs.sh
@@ -10,18 +10,22 @@ path="$1"
 
 BASE_OLD="#00FF00"
 OUTLINE_OLD="#0000FF"
+WATCH_OLD="#FF0000"
 
-BASE_NEW="#4D4D4D"
+BASE_NEW="#FF8300"
 OUTLINE_NEW="#FFFFFF"
+WATCH_NEW="#001524"
 
 find "$path" -type f -name "*.svg" | while read -r file; do
 	echo "Processing: $file"
 
 	sed -i "s/$BASE_OLD/tmp001/g" "$file"
 	sed -i "s/$OUTLINE_OLD/tmp002/g" "$file"
+	sed -i "s/$WATCH_OLD/tmp003/g" "$file"
 
 	sed -i "s/tmp001/$BASE_NEW/g" "$file"
 	sed -i "s/tmp002/$OUTLINE_NEW/g" "$file"
+	sed -i "s/tmp003/$WATCH_NEW/g" "$file"
 done
 
 echo "Processing done!"
